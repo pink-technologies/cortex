52b456c936dc1513e152383d54944a771cf52152
emily-machuca-pink <emily.machuca@pink-tech.io>
2026-04-28 16:18:26 -0500

    feat: Initialize organization module with base structure

---
+++ src/organizations/services/roles/organization.roles.service.ts
@@ -0,0 +1,55 @@
+// Copyright (c) 2026, PinkTech
+// https://pink-tech.io/
+
+import { OrganizationRole } from "@/infraestructure/database";
+import { RoleType } from "@prisma/client";
+import { Injectable } from "@nestjs/common";
+import { OrganizationRolesRepository } from "../../repositories";
+import { RoleNotFound } from "../error/organization.error";
+
+/**
+ * Service responsible for managing organization roles.
+ *
+ * This service provides operations to find organization roles by type and retrieve all organization roles.
+ */
+@Injectable()
+export class OrganizationRolesService {
+    // MARK: - Constructor
+
+    /**
+     * Creates a new {@link OrganizationRoles}.
+     * 
+     * @param organizationRolesRepository - The repository responsible for querying organization role entities.
+     */
+    constructor(private readonly organizationRolesRepository: OrganizationRolesRepository) { }
+
+    // MARK: - Instance methods
+
+    /**
+     * Finds an organization role by its role type within an organization.
+     *
+     * @param organizationId - The organization that owns the role.
+     * @param roleType - The role type to look up.
+     * @returns The matching role or null if not found.
+     */
+    async findByRoleType(roleType: RoleType): Promise<OrganizationRole> {
+        if (!Object.values(RoleType).includes(roleType)) {
+            throw new RoleNotFound();
+        }
+
+        const role = await this.organizationRolesRepository.findByRoleType(roleType);
+
+        if (!role) throw new RoleNotFound();
+
+        return role;
+    }
+
+    /**
+     * Retrieves all organization roles.
+     *
+     * @returns A list of all organization roles.
+     */
+    async retrieve(): Promise<OrganizationRole[]> {
+        return await this.organizationRolesRepository.retrieve();
+    }
+}
