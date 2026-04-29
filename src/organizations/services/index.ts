16baf21fd4d668e0700aaadcda8c29cb19dead85
emily-machuca-pink <emily.machuca@pink-tech.io>
2026-04-29 14:47:17 -0500

    pr feedback

--- src/organizations/services/index.ts
+++
@@ -1,5 +0,0 @@
-export { OrganizationRolesService } from './roles/organization.roles.service';
-export {
-    type CreateOrganizationParameters,
-    OrganizationsService,
-} from './organizations/organizations.service';
