f285b28d36de957ad3a3fee2a37052b9081966bb
emily-machuca-pink <emily.machuca@pink-tech.io>
2026-04-29 14:47:17 -0500

    pr feedback

--- src/organizations/repositories/index.ts
+++
@@ -1,3 +0,0 @@
-export { OrganizationMembershipsRepository } from './memberships/organization-memberships.repository';
-export { OrganizationsRepository } from './organizations/organizations.repository';
-export { OrganizationRolesRepository } from './roles/organization-roles.repository';
